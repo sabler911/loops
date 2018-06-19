7ba66e45ab26c8238e589c779cff34c082a42ed2
sabler911 <mr_wolf20011@yahoo.com>
2018-06-18 23:16:25 -0400

    First Comment
    Uploading All files

---
+++ Loops/src/Application2.java
@@ -0,0 +1,15 @@
+
+public class Application2 {
+	public static void main(String[] args) {
+		
+		int value = 0;
+		
+		while(value < 40)
+		{
+			System.out.println("I'm tired " + value);
+			
+			value = value + 2;
+		}
+	}
+
+}
